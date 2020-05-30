decae8187440ae40ea06542e6928503f597f6be6
Livenow14 <xowjd41@naver.com>
2020-05-30 13:57:43 +0900

    feat: Save spot with SpotAdmin_id

--- src/main/java/org/quickresponse/qr/controller/SpotController.java
+++ src/main/java/org/quickresponse/qr/controller/SpotController.java
@@ -22,9 +22,10 @@ public class SpotController {
     private final SpotService spotService;
     private final SpotRepository spotRepository;
 
-    @PostMapping("/")                                       //새로운 spot 생성
-    public SpotSaveResponseDto save(@RequestBody SpotSaveRequestDto spotSaveRequestDto){
-        Long joinId = spotService.join(spotSaveRequestDto);
+    @PostMapping("/{spotAdminId}")                                       //새로운 spot 생성
+    public SpotSaveResponseDto save(@PathVariable("spotAdminId") Long spotAdminId,
+                                    @RequestBody SpotSaveRequestDto spotSaveRequestDto){
+        Long joinId = spotService.join(spotSaveRequestDto, spotAdminId);
         return new SpotSaveResponseDto(joinId);
     }
 
--- src/main/java/org/quickresponse/qr/service/SpotService.java
+++ src/main/java/org/quickresponse/qr/service/SpotService.java
@@ -1,7 +1,9 @@
 package org.quickresponse.qr.service;
 
 import lombok.RequiredArgsConstructor;
+import org.quickresponse.qr.domain.Spot;
 import org.quickresponse.qr.dto.SpotSaveRequestDto;
+import org.quickresponse.qr.repository.SpotAdminRepository;
 import org.quickresponse.qr.repository.SpotRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,9 +14,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class SpotService {
 
     private final SpotRepository spotRepository;
+    private final SpotAdminRepository spotAdminRepository;
 
     @Transactional
-    public Long join(SpotSaveRequestDto spotSaveRequestDto) {
-        return spotRepository.join(spotSaveRequestDto.toEntity());
+    public Long join(SpotSaveRequestDto spotSaveRequestDto, Long spotAdminId) {
+        Spot spot = spotSaveRequestDto.toEntity();
+        spot.setSpotAdmin(spotAdminRepository.find(spotAdminId));
+        return spotRepository.join(spot);
     }
 }
