75e9b6cc6c6b1e55527ad45821bb8f0a2806c4c4
PandaHun <gnsgnsek@gmail.com>
2020-06-05 18:18:56 +0900

    refactor: modify login Controller

--- src/main/java/org/quickresponse/qr/controller/LoginController.java
+++ src/main/java/org/quickresponse/qr/controller/LoginController.java
@@ -3,13 +3,9 @@ package org.quickresponse.qr.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quickresponse.qr.service.common.dto.TokenResponse;
-<<<<<<< HEAD:src/main/java/org/quickresponse/qr/controller/LoginController.java
 import org.quickresponse.qr.service.spotAdmin.SpotAdminService;
 import org.quickresponse.qr.service.spotAdmin.dto.SpotAdminLoginDto;
-import org.quickresponse.qr.service.user.user.UserLoginDto;
-=======
 import org.quickresponse.qr.service.user.dto.UserLoginDto;
->>>>>>> 0a88a2c0c989b04ec2babc9180b4d75a447a1a63:src/main/java/org/quickresponse/qr/controller/LoginUserController.java
 import org.quickresponse.qr.service.user.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/cfcqr/api/login")
-public class LoginUserController {
+public class LoginController {
 
     private final UserService userService;
     private final SpotAdminService spotAdminService;
