2c29b2eef9807d88ad5cc48c285e4af06f27427c
Livenow14 <xowjd41@naver.com>
2020-06-09 21:56:52 +0900

    feat: fcm 추가(영어만)

---
+++ src/main/java/org/quickresponse/qr/service/fcm/AndroidPushNotificationsService.java
@@ -0,0 +1,34 @@
+package org.quickresponse.qr.service.fcm;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
+
+// firebase_server_key = firebase project > cloud messaging > server key
+
+@Service
+public class AndroidPushNotificationsService {
+    private static final String firebase_server_key="AAAA0PQ7mtI:APA91bHcdNWh5uGB65TAMLt8DF0s49CpC5Yu-C-zK12f-qDyQmCWy-oXrpIRT5JbPwti4nFKYnr04XPm_Q02wwcvK-WvMxbIYUqkA9EDxsXVAHzVPH4YrPcqKsQmQh59eYgAaUIE8aaE";
+    private static final String firebase_api_url="https://fcm.googleapis.com/fcm/send";
+
+    @Async
+    public CompletableFuture<String> send(HttpEntity<String> entity) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ArrayList<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+
+        interceptors.add(new HeaderRequestInterceptor("Authorization",  "key=" + firebase_server_key));
+        interceptors.add(new HeaderRequestInterceptor("Content-Type", "application/json; charset=UTF-8"));
+        restTemplate.setInterceptors(interceptors);
+
+        String firebaseResponse = restTemplate.postForObject(firebase_api_url, entity, String.class);
+
+        return CompletableFuture.completedFuture(firebaseResponse);
+    }
+}
