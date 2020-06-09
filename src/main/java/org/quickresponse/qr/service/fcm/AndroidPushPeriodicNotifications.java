abfcb48036b798681bd4cde753a8b8c5fdae62d8
Livenow14 <xowjd41@naver.com>
2020-06-10 01:12:03 +0900

    feat: fcm 추가

---
+++ src/main/java/org/quickresponse/qr/service/fcm/AndroidPushPeriodicNotifications.java
@@ -0,0 +1,88 @@
+package org.quickresponse.qr.service.fcm;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import java.net.URLEncoder;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AndroidPushPeriodicNotifications {
+
+    public static String PeriodicNotificationJson() throws JSONException, UnsupportedEncodingException {
+        LocalDate localDate = LocalDate.now();
+
+        String sampleData[] = {"device_token"};
+
+        JSONObject body = new JSONObject();
+
+        List<String> tokenlist = new ArrayList<String>();
+
+        for(int i=0; i<sampleData.length; i++){
+            tokenlist.add(sampleData[i]);
+        }
+
+        JSONArray array = new JSONArray();
+
+        for(int i=0; i<tokenlist.size(); i++) {
+            array.put(tokenlist.get(i));
+        }
+
+        body.put("registration_ids", array);
+
+        JSONObject notification = new JSONObject();
+        notification.put("title", URLEncoder.encode("제성","UTF-8"));
+        notification.put("body", URLEncoder.encode("진짜 안녕! ","UTF-8"));
+
+        body.put("notification", notification);
+
+        System.out.println(body.toString());
+
+        return body.toString();
+    }
+
+   /* public static String alarmSaveNotification(AlarmSaveRequestDto alarmSaveRequestDto) throws JSONException {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        JSONObject notification = new JSONObject();
+        JSONObject body = new JSONObject();
+
+        String sampleData[] = {"device token value 1","device token value 2","device token value 3"};
+
+        List<String> tokenlist = new ArrayList<>();
+
+        for(int i=0; i<sampleData.length; i++){
+            tokenlist.add(sampleData[i]);
+        }
+
+        JSONArray array = new JSONArray();
+
+        for(int i=0; i<tokenlist.size(); i++) {
+            array.put(tokenlist.get(i));
+        }
+
+        body.put("registration_ids", array);
+
+        if(alarmSaveRequestDto.getDecibel()!=null){
+            notification.put("title"," Decibel warning!!");
+            notification.put("body","Decibel : " + alarmSaveRequestDto.getDecibel());
+            System.out.println(notification.toString());
+        }
+        if(alarmSaveRequestDto.getHeart()!=null){
+            notification.put("title"," Heart warning!!");
+            notification.put("body","Heart : " + alarmSaveRequestDto.getHeart());
+            System.out.println(notification.toString());
+        }
+        if(alarmSaveRequestDto.getTumble()!=null){
+            notification.put("title"," Tumble warning!!");
+            notification.put("body","Tumble : " + alarmSaveRequestDto.getTumble());
+            System.out.println(notification.toString());
+        }
+
+        body.put("notification", notification);
+        return body.toString();
+    }*/
+}
---
+++ src/main/java/org/quickresponse/qr/service/fcm/NotificationController.java
@@ -0,0 +1,50 @@
+package org.quickresponse.qr.service.fcm;
+
+import org.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+public class NotificationController {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    AndroidPushNotificationsService androidPushNotificationsService;
+
+    @GetMapping(value = "/send")
+    public @ResponseBody
+    ResponseEntity<String> send() throws JSONException, InterruptedException, UnsupportedEncodingException {
+        String notifications = AndroidPushPeriodicNotifications.PeriodicNotificationJson();
+
+        HttpEntity<String> request = new HttpEntity<>(notifications);
+
+        CompletableFuture<String> pushNotification = androidPushNotificationsService.send(request);
+        CompletableFuture.allOf(pushNotification).join();
+
+        try{
+            String firebaseResponse = pushNotification.get();
+            return new ResponseEntity<>(firebaseResponse, HttpStatus.OK);
+        }
+        catch (InterruptedException e){
+            logger.debug("got interrupted!");
+            throw new InterruptedException();
+        }
+        catch (ExecutionException e){
+            logger.debug("execution error!");
+        }
+
+        return new ResponseEntity<>("Push Notification ERROR!", HttpStatus.BAD_REQUEST);
+    }
+}
